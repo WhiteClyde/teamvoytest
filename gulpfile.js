a5d42f0c39b2adf261ef2140c8f99e1e33b3b927
Vadym Yarovoi <yarovy.vadym@gmail.com>
2017-10-06 14:49:29 +0300

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ const gulp         = require('gulp'),
       sass         = require('gulp-sass'),
       browserSync  = require('browser-sync'),
       babel        = require('gulp-babel'),
-      autoprefixer = require('gulp-autoprefixer');
+      autoprefixer = require('gulp-autoprefixer'),
+      concat       = require('gulp-concat'),
+      uglify       = require('gulp-uglifyjs');
 
 gulp.task('sass', () => {
 return gulp.src('src/sass/**/*.sass')
@@ -29,6 +31,17 @@ return gulp.src('src/js/src/*.js')
          .pipe(gulp.dest('src/js/dest'));
 });
 
+gulp.task('scripts', () => {
+    return gulp.src([
+        'src/libs/jquery/dist/jquery.min.js',
+        'src/libs/jPlayer/dist/jplayer/jquery.jplayer.min.js',
+        'src/libs/jPlayer/dist/add-on/jplayer.playlist.min.js'
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/js'));
+});
+
 gulp.task('clean', () => {
     return del.sync('dist');
 });
@@ -40,12 +53,12 @@ gulp.task('watch', ['browser-sync', 'es6'], () => {
     gulp.watch('src/js/**/*.js', browserSync.reload);
 });
 
-gulp.task('build', ['clean', 'sass', 'es6'], () => {
-let buildCss = gulp.src('src/css/style.css')
+gulp.task('build', ['clean', 'sass', 'es6', 'scripts'], () => {
+let buildCss = gulp.src('src/css/**/*')
     .pipe(gulp.dest('dist/css'));
 
-let buildJs = gulp.src(['src/js/dest/*.js', 'src/libs/*.js'])
-    .pipe(gulp.dest('dist/js/dest'));
+let buildJs = gulp.src(['src/js/dest/*.js', 'src/js/libs.min.js'])
+    .pipe(gulp.dest('dist/js'));
 
 let buildHtml = gulp.src('src/*.html')
     .pipe(gulp.dest('dist'));
